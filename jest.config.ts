bb66bdbc7a0e19875e637f1931da73fcfee760a3
Dom Bano <dombano@users.noreply.github.com>
2026-05-06 03:15:14 +0800

    test: add minimum coverage thresholds to jest config
    fixes #1

--- jest.config.ts
+++ jest.config.ts
@@ -8,6 +8,14 @@ const config: Config = {
   collectCoverageFrom: ['src/**/*.ts', '!src/**/*.test.ts'],
   coverageDirectory: 'coverage',
   coverageReporters: ['text', 'lcov'],
+  coverageThreshold: {
+    global: {
+      branches: 80,
+      functions: 80,
+      lines: 80,
+      statements: 80,
+    },
+  },
   verbose: true,
 };
 
